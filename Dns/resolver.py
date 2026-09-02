678fefadb6e522f4b953177de6efdb26a8bf9b1b
Chvrd0 <ricardo.roman@ug.uchile.cl>
2026-09-01 23:44:57 -0400

    Feat: Resolver puede recibir mensajes

--- Dns/resolver.py
+++ Dns/resolver.py
@@ -1,1 +1,16 @@
+import socket
 IP_VM = "localhost"
+
+if __name__ == "__main__":
+    buff_size = 10000
+    address = ('localhost', 8000)
+
+    print('Creando socket - Servidor')
+
+    server_socket =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(address)
+
+    while True:
+        msg, remitente = server_socket.recvfrom(buff_size)
+
+        print(f" -> Se ha recibido con éxito el mensaje: \n -> Mensaje: {msg}\n -> Desde {remitente}")
